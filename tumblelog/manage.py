80acd8991f517e197c62247bb182b19bd006021a
woosungchu <woosungchu@gmail.com>
2016-06-18 01:31:59 +0900

    23

--- tumblelog/manage.py
+++ tumblelog/manage.py
@@ -11,5 +11,5 @@ from tumblelog import app
 if __name__ == "__main__":
 #    capture_all_args = True
 #    manager.run()
-#    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=$PORT)
+    port = int(os.environ.get('$PORT', 5000)) or 5000
+    app.run(host='0.0.0.0',port=port)
