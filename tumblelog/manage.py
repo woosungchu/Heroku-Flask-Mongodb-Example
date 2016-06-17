5546eba7c64597bdbd92f3dd9e483687d6d76682
woosungchu <woosungchu@gmail.com>
2016-06-17 23:11:04 +0900

    script fuc you

--- tumblelog/manage.py
+++ tumblelog/manage.py
@@ -4,10 +4,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from flask_script import Manager, Server
 from tumblelog import app
 
-manager = Manager(app)
+#manager = Manager(app)
 
-manager.add_command("runserver", Server(use_debugger = True , use_reloader=True, host='0.0.0.0', port=5000),)
+#manager.add_command("runserver", Server(use_debugger = True , use_reloader=True, host='0.0.0.0', port=5000),)
 
 if __name__ == "__main__":
-    capture_all_args = True
-    manager.run()
+#    capture_all_args = True
+#    manager.run()
+    app.run()
