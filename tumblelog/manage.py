0ec967d96a832d91f89a81af381eb87df7473f21
woosungchu <woosungchu@gmail.com>
2016-06-18 14:06:31 +0900

    asdasd

--- tumblelog/manage.py
+++ tumblelog/manage.py
@@ -6,7 +6,13 @@ from tumblelog import app
 
 manager = Manager(app)
 
-manager.add_command("runserver", Server(use_debugger = True , use_reloader=True, host='0.0.0.0'))
+manager.add_command("runserver", Server(use_debugger = True , use_reloader=True, host='0.0.0.0', port=5000),)
 
 if __name__ == "__main__":
-    manager.run()
+#    app.debug = app.config['DEBUG']
+#    capture_all_args = True
+#    manager.run()
+#    port = int(os.environ.get('$PORT')) or 30133
+#    port = int(os.getenv('$PORT',5000))
+#    app.run(host='0.0.0.0',port=port)
+     app.run()
